162d542c4474ac98b70d5771902ccf2ce698f234
dszarkiel <d.szarkiel@gmail.com>
2020-12-29 10:29:51 -0600

    commit tree 4ca2aebc21a511290a4faa85fe6109669913b825

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1
+
+let mod = 14 % 5 
+
+let max = Math.max(1, 5, 6, 12, 19, 20)
+
